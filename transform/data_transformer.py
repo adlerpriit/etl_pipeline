e7b37b4598534ebc49fb06ccc6d238879e803f4b
adler <adler.priit@gmail.com>
2023-05-18 14:41:04 +0300

    change all numeric data to float type

---
+++ transform/data_transformer.py
@@ -0,0 +1,28 @@
+def transform_data(data):
+    """
+    Transforms a list of lists by converting all numerical values to floats.
+    Assumes the input is a table of data, where each sub-list is a row.
+    """
+    transformed_data = []
+    
+    for row in data:
+        new_row = []
+        for value in row:
+            try:
+                new_row.append(float(value))
+            except ValueError:
+                new_row.append(value)
+        transformed_data.append(new_row)
+    
+    return transformed_data
+
+if __name__ == "__main__":
+    data = [
+        ["name", "age", "weight"],
+        ["John Doe", "30", "75.5"],
+        ["Jane Doe", "25", "68.2"],
+    ]
+    
+    transformed_data = transform_data(data)
+    for row in transformed_data:
+        print(row)

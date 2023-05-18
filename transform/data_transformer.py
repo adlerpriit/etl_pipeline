d04743d18c54142acb9d34568deb1b5994d2f89f
adler <adler.priit@gmail.com>
2023-05-18 15:06:28 +0300

    bad example of code for conflict

--- transform/data_transformer.py
+++ transform/data_transformer.py
@@ -26,3 +26,30 @@ if __name__ == "__main__":
     transformed_data = transform_data(data)
     for row in transformed_data:
         print(row)
+
+
+import csv
+
+def transform_and_write_data(data):
+    transformed_data = []
+    for row in data:
+        new_row = []
+        for value in row:
+            try:
+                new_row.append(float(value))
+            except ValueError:
+                new_row.append(value)
+        transformed_data.append(new_row)
+    
+    with open('output.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(transformed_data)
+
+if __name__ == "__main__":
+    data = [
+        ["name", "age", "weight"],
+        ["John Doe", "30", "75.5"],
+        ["Jane Doe", "25", "68.2"],
+    ]
+    
+    transform_and_write_data(data)

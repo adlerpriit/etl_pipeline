beaba6c92230e042ea873e5f9570608db2be8932
adler <adler.priit@gmail.com>
2023-05-18 15:47:13 +0300

    added test to the new transformation as well

--- tests/test_data_transformer.py
+++ tests/test_data_transformer.py
@@ -1,5 +1,9 @@
 import unittest
 from data_transformer import transform_data
+from data_transformer import standardize_data
+import numpy as np
+import pandas as pd
+
 
 class TestDataTransformer(unittest.TestCase):
     
@@ -22,6 +26,26 @@ class TestDataTransformer(unittest.TestCase):
 
         # Assert that the actual output matches the expected output
         self.assertEqual(actual_output, expected_output)
+
+def test_standardize_data():
+    # Create a sample DataFrame
+    data = {
+        'A': [1, 2, 3, 4, 5],
+        'B': [100, 200, 300, 400, 500],
+        'C': ['a', 'b', 'c', 'd', 'e']
+    }
+    df = pd.DataFrame(data)
+
+    # Standardize the data
+    df_standardized = standardize_data(df.copy())
+
+    # Check that the numeric columns have mean 0 and std deviation 1
+    for col in ['A', 'B']:
+        assert np.isclose(df_standardized[col].mean(), 0)
+        assert np.isclose(df_standardized[col].std(), 1)
+
+    # Check that the non-numeric column has not changed
+    assert df_standardized['C'].equals(df['C'])
 
 
 if __name__ == "__main__":
